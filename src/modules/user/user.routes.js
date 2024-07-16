7983046b69ec666cbf5d91bf29230463af5fab72
omar fetooh <omarfetooh62@gmail.com>
2024-07-16 16:15:44 +0300

    Add user Validations

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,19 +1,28 @@
 import { Router } from "express";
 import {
-    deleteAccount, getAllAccsOfRecovEmail, getProfileData, getUserAccountData,
+    deleteAccount, getAllAccsOfRecovEmail,
+    getProfileData, getUserAccountData,
     requestResetPassword, resetPassword,
-    signin, signup, updateAccount,
-    updatePassword,
+    signin, signup,
+    updateAccount, updatePassword,
 } from "./user.controllers.js";
 
 import userMiddleware from "./user.middleware.js";
 import { auth } from "../../middlewares/auth.middleware.js";
 import upload from "../../utils/upload.js";
+import validate from '../../middlewares/validate.middleware.js'
+import {
+    resetPasswordSchema,
+    signinSchema,
+    signupSchema,
+    updateAccountSchema,
+    updatePasswordSchmea
+} from "./user.validations.js";
 
 const userRouter = Router();
 
-userRouter.post('/signup', signup)
-userRouter.post('/signin', signin)
+userRouter.post('/signup', validate(signupSchema), signup)
+userRouter.post('/signin', validate(signinSchema), signin)
 
 userRouter
     .route('/account')
@@ -21,15 +30,15 @@ userRouter
 
 
 userRouter.route('/:userId')
-    .put(auth('User'), userMiddleware, updateAccount)
+    .put(auth('User'), validate(updateAccountSchema), userMiddleware, updateAccount)
     .delete(auth('User'), userMiddleware, deleteAccount)
     .get(auth('User'), userMiddleware, getUserAccountData)
 
 
 userRouter.put('/account/:userId', auth('User'), userMiddleware, updatePassword)
 
-userRouter.post('/requestResetPassword', requestResetPassword)
-userRouter.post('/resetPassword', resetPassword)
+userRouter.post('/requestResetPassword', validate(updatePasswordSchmea), requestResetPassword)
+userRouter.post('/resetPassword', validate(resetPasswordSchema), resetPassword)
 
 userRouter.get('/getAllAccsOfRecovEmail/:recoveryEmail', getAllAccsOfRecovEmail)
 
---
+++ src/modules/user/user.validations.js
@@ -0,0 +1,41 @@
+import Joi from 'joi'
+
+export const signupSchema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    recoveryEmail: Joi.string().required(),
+    DOB: Joi.date().required(),
+    mobileNumber: Joi.string().required(),
+    role: Joi.string()
+})
+
+export const signinSchema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+})
+
+export const updateAccountSchema = Joi.object({
+    email: Joi.string(),
+    mobileNumber: Joi.string(),
+    recoveryEmail: Joi.string(),
+    DOB: Joi.date(),
+    lastName: Joi.string(),
+    firstName: Joi.string()
+})
+
+export const requestResetPasswordSchema = Joi.object({
+    email: Joi.string().required()
+})
+
+export const resetPasswordSchema = Joi.object({
+    email: Joi.string().required(),
+    otp: Joi.string().required(),
+    newPassword: Joi.string().required()
+})
+
+export const updatePasswordSchmea = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+})
